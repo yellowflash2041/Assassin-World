discourse/discourse

FEATURE: Webhook for user creation and approval

app/models/web_hook.rb
@@ -67,6 +67,12 @@ class WebHook < ActiveRecord::Base
       )
     end
   end
+
+  %i(user_created user_approved).each do |event|
+    DiscourseEvent.on(event) do |user|
+      WebHook.enqueue_hooks(:user_creation, user_id: user.id, event_name: event.to_s)
+    end
+  end
 end
 
 # == Schema Information

db/fixtures/007_web_hook_event_types.rb
@@ -7,3 +7,8 @@ WebHookEventType.seed do |b|
   b.id = WebHookEventType::POST
   b.name = "post"
 end
+
+WebHookEventType.seed do |b|
+  b.id = WebHookEventType::USER_CREATION
+  b.name = "user_creation"
+end

yellowflash2041/Assassin-World

app/models/web_hook.rb
@@ -68,6 +68,12 @@ class WebHook < ActiveRecord::Base
       )
     end
   end
+
+  %i(user_created user_approved).each do |event|
+    DiscourseEvent.on(event) do |user|
+      WebHook.enqueue_hooks(:user, user_id: user.id, event_name: event.to_s)
+    end
+  end
 end
 
 # == Schema Information

db/fixtures/007_web_hook_event_types.rb
@@ -7,3 +7,8 @@ WebHookEventType.seed do |b|
   b.id = WebHookEventType::POST
   b.name = "post"
 end
+
+WebHookEventType.seed do |b|
+  b.id = WebHookEventType::USER
+  b.name = "user"
+end