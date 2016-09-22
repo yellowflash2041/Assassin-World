ajodock/discourse

Scaffold for new Wizard - Rails / Ember / Tests

app/assets/javascripts/wizard/routes/index.js.es6
@@ -1,0 +1,5 @@
+export default Ember.Route.extend({
+  beforeModel() {
+    this.replaceWith('steps', 'welcome');
+  }
+});

yellowflash2041/Assassin-World

app/assets/javascripts/wizard/routes/index.js.es6
@@ -1,0 +1,5 @@
+export default Ember.Route.extend({
+  beforeModel() {
+    this.replaceWith('step', 'welcome');
+  }
+});