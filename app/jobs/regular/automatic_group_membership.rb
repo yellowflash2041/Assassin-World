discourse/discourse

FEATURE: phase 1 of supporting multiple email addresses

app/jobs/regular/automatic_group_membership.rb
@@ -13,7 +13,8 @@ module Jobs
 
       domains = group.automatic_membership_email_domains.gsub('.', '\.')
 
-      User.where("email ~* '@(#{domains})$'")
+      User.joins(:primary_email)
+          .where("user_emails.email ~* '@(#{domains})$'")
           .where("users.id NOT IN (SELECT user_id FROM group_users WHERE group_users.group_id = ?)", group_id)
           .activated
           .where(staged: false)

yellowflash2041/Assassin-World

app/jobs/regular/automatic_group_membership.rb
@@ -13,7 +13,8 @@ module Jobs
 
       domains = group.automatic_membership_email_domains.gsub('.', '\.')
 
-      User.where("email ~* '@(#{domains})$'")
+      User.joins(:user_emails)
+          .where("user_emails.email ~* '@(#{domains})$'")
           .where("users.id NOT IN (SELECT user_id FROM group_users WHERE group_users.group_id = ?)", group_id)
           .activated
           .where(staged: false)