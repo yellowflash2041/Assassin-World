discourse/discourse

commit e83c2488a2d9c686fde83cef93878ee4a9d0f119

FIX: do not unpin reply box in iOS when selecting Emoji

app/assets/javascripts/discourse/lib/safari-hacks.js.es6
@@ -117,6 +117,7 @@ function positioningWorkaround($fixedElement) {
     if (
       lastTouchedElement &&
       ($(lastTouchedElement).hasClass("select-kit-header") ||
+        $(lastTouchedElement).closest(".autocomplete") ||
         ["span", "svg", "button"].includes(
           lastTouchedElement.nodeName.toLowerCase()
         ))

yellowflash2041/Assassin-World

commit 15631f0c44087bb94c5adced90be7ec06d2216ec

UX: fixes regression with iOS composer
Fixes a bug introduced in e83c2488a2d9c686fde83cef93878ee4a9d0f119 where dismissing the keyboard in iOS would not minimize the composer.

app/assets/javascripts/discourse/lib/safari-hacks.js.es6
@@ -117,7 +117,7 @@ function positioningWorkaround($fixedElement) {
     if (
       lastTouchedElement &&
       ($(lastTouchedElement).hasClass("select-kit-header") ||
-        $(lastTouchedElement).closest(".autocomplete") ||
+        $(lastTouchedElement).closest(".autocomplete").length ||
         ["span", "svg", "button"].includes(
           lastTouchedElement.nodeName.toLowerCase()
         ))