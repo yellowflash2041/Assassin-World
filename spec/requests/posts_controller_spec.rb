discourse/discourse

commit e8fb9d406667c758cba0f7b1cc779e1ceb352c4c

FIX: when creating new PM username/groupname should be case-insensitive
(take 2)

https://meta.discourse.org/t/case-sensitivity-in-links-to-groupname/147596
https://meta.discourse.org/t/remove-case-sensitive-in-adding-users-to-a-message/151275

app/controllers/posts_controller.rb
@@ -747,8 +747,8 @@ class PostsController < ApplicationController
     end
 
     if recipients
-      recipients = recipients.split(",")
-      groups = Group.messageable(current_user).where('name in (?)', recipients).pluck('name')
+      recipients = recipients.split(",").map(&:downcase)
+      groups = Group.messageable(current_user).where('lower(name) in (?)', recipients).pluck('name')
       recipients -= groups
       emails = recipients.select { |user| user.match(/@/) }
       recipients -= emails

lib/topic_creator.rb
@@ -208,10 +208,10 @@ class TopicCreator
   def add_users(topic, usernames)
     return unless usernames
 
-    names = usernames.split(',').flatten
+    names = usernames.split(',').flatten.map(&:downcase)
     len = 0
 
-    User.includes(:user_option).where(username: names).find_each do |user|
+    User.includes(:user_option).where('lower(username) in (?)', names).find_each do |user|
       check_can_send_permission!(topic, user)
       @added_users << user
       topic.topic_allowed_users.build(user_id: user.id)

spec/requests/posts_controller_spec.rb
@@ -825,7 +825,7 @@ describe PostsController do
         post "/posts.json", params: {
           raw: 'I can haz a test',
           title: 'I loves my test',
-          target_recipients: group.name,
+          target_recipients: "test_Group",
           archetype: Archetype.private_message
         }
 
@@ -965,13 +965,13 @@ describe PostsController do
 
       it 'creates a private post' do
         user_2 = Fabricate(:user)
-        user_3 = Fabricate(:user)
+        user_3 = Fabricate(:user, username: "foo_bar")
 
         post "/posts.json", params: {
           raw: 'this is the test content',
           archetype: 'private_message',
           title: "this is some post",
-          target_recipients: "#{user_2.username},#{user_3.username}"
+          target_recipients: "#{user_2.username},Foo_Bar"
         }
 
         expect(response.status).to eq(200)

yellowflash2041/Assassin-World

commit 4af086d29cee7b5bfeb003d58820bdbfcbdd4781

FIX: sending messages to groups with non-lowercase names
Fixes a regression in

e8fb9d406667c758cba0f7b1cc779e1ceb352c4c

which caused a bug where you couldn't send a message to a group that
contained an Uppercase letter. Added a test case for this.

Bug report: https://meta.discourse.org/t/-/152999

app/controllers/posts_controller.rb
@@ -748,7 +748,7 @@ class PostsController < ApplicationController
 
     if recipients
       recipients = recipients.split(",").map(&:downcase)
-      groups = Group.messageable(current_user).where('lower(name) in (?)', recipients).pluck('name')
+      groups = Group.messageable(current_user).where('lower(name) in (?)', recipients).pluck('lower(name)')
       recipients -= groups
       emails = recipients.select { |user| user.match(/@/) }
       recipients -= emails

lib/topic_creator.rb
@@ -246,10 +246,10 @@ class TopicCreator
 
   def add_groups(topic, groups)
     return unless groups
-    names = groups.split(',').flatten
+    names = groups.split(',').flatten.map(&:downcase)
     len = 0
 
-    Group.where(name: names).each do |group|
+    Group.where('lower(name) in (?)', names).each do |group|
       check_can_send_permission!(topic, group)
       topic.topic_allowed_groups.build(group_id: group.id)
       len += 1

spec/requests/posts_controller_spec.rb
@@ -838,6 +838,30 @@ describe PostsController do
         expect(post.topic.topic_allowed_groups.length).to eq(1)
       end
 
+      it "can send a message to a group with caps" do
+        group = Group.create(name: 'Test_group', messageable_level: Group::ALIAS_LEVELS[:nobody])
+        user1 = user
+        group.add(user1)
+
+        # allow pm to this group
+        group.update_columns(messageable_level: Group::ALIAS_LEVELS[:everyone])
+
+        post "/posts.json", params: {
+          raw: 'I can haz a test',
+          title: 'I loves my test',
+          target_recipients: "test_Group",
+          archetype: Archetype.private_message
+        }
+
+        expect(response.status).to eq(200)
+
+        parsed = response.parsed_body
+        post = Post.find(parsed['id'])
+
+        expect(post.topic.topic_allowed_users.length).to eq(1)
+        expect(post.topic.topic_allowed_groups.length).to eq(1)
+      end
+
       it "returns the nested post with a param" do
         post "/posts.json", params: {
           raw: 'this is the test content',