oreoshake/discourse

REFACTOR: Move Page title / focus / counts logic to service
We had a handful of methods attached to the root `Discourse` object
related to focus and notification counts.

This patch pulls them out into a service called `document-title` for
updating the title, and a component called `d-document` to attach
and listen for browser events related to focus.

It also removes some computed properties and observers in favor of plain
old Javascript objects.

app/assets/javascripts/discourse/app/components/d-document.js
@@ -1,0 +1,58 @@
+import Component from "@ember/component";
+import { bind } from "@ember/runloop";
+import { inject as service } from "@ember/service";
+
+export default Component.extend({
+  _boundFocusChange: null,
+  tagName: "",
+  documentTitle: service(),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.documentTitle.setTitle(document.title);
+    this._boundFocusChange = bind(this, this._focusChanged);
+    document.addEventListener("visibilitychange", this._boundFocusChange);
+    document.addEventListener("resume", this._boundFocusChange);
+    document.addEventListener("freeze", this._boundFocusChange);
+    this.session.hasFocus = true;
+
+    this.appEvents.on("notifications:changed", this, this._updateNotifications);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+
+    document.removeEventListener("visibilitychange", this._boundFocusChange);
+    document.removeEventListener("resume", this._boundFocusChange);
+    document.removeEventListener("freeze", this._boundFocusChange);
+    this._boundFocusChange = null;
+
+    this.appEvents.off(
+      "notifications:changed",
+      this,
+      this._updateNotifications
+    );
+  },
+
+  _updateNotifications() {
+    if (!this.currentUser) {
+      return;
+    }
+
+    this.documentTitle.updateNotificationCount(
+      this.currentUser.unread_notifications +
+        this.currentUser.unread_high_priority_notifications
+    );
+  },
+
+  _focusChanged() {
+    if (document.visibilityState === "hidden") {
+      if (this.session.hasFocus) {
+        this.documentTitle.updateFocus(false);
+      }
+    } else if (!this.hasFocus) {
+      this.documentTitle.updateFocus(true);
+    }
+  }
+});

app/assets/javascripts/discourse/app/services/document-title.js
@@ -1,0 +1,105 @@
+import Service from "@ember/service";
+import { inject as service } from "@ember/service";
+import getURL from "discourse-common/lib/get-url";
+
+export default Service.extend({
+  appEvents: service(),
+  contextCount: null,
+  notificationCount: null,
+  _title: null,
+  _backgroundNotify: null,
+
+  init() {
+    this._super(...arguments);
+    this.reset();
+  },
+
+  reset() {
+    this.contextCount = 0;
+    this.notificationCount = 0;
+    this._title = null;
+    this._backgroundNotify = null;
+  },
+
+  getTitle() {
+    return this._title;
+  },
+
+  setTitle(title) {
+    this._title = title;
+    this._renderTitle();
+  },
+
+  updateFocus(focus) {
+    let { session } = this;
+
+    session.hasFocus = focus;
+
+    if (session.hasFocus && this._backgroundNotify) {
+      this.updateContextCount(0);
+    }
+    this._backgroundNotify = false;
+
+    if (session.hasFocus) {
+      this.notificationCount = 0;
+    }
+    this.appEvents.trigger("discourse:focus-changed", session.hasFocus);
+    this._renderTitle();
+  },
+
+  updateContextCount(count) {
+    this.contextCount = count;
+    this._renderTitle();
+  },
+
+  updateNotificationCount(count) {
+    if (!this.session.hasFocus) {
+      this.notificationCount = count;
+      this._renderFavicon();
+      this._renderTitle();
+    }
+  },
+
+  incrementBackgroundContextCount() {
+    if (!this.session.hasFocus) {
+      this._backgroundNotify = true;
+      this.contextCount += 1;
+      this._renderFavicon();
+      this._renderTitle();
+    }
+  },
+
+  _displayCount() {
+    return this.currentUser &&
+      this.currentUser.get("title_count_mode") === "notifications"
+      ? this.notificationCount
+      : this.contextCount;
+  },
+
+  _renderTitle() {
+    let title = this._title || this.siteSettings.title;
+
+    let displayCount = this._displayCount();
+    let dynamicFavicon = this.currentUser && this.currentUser.dynamic_favicon;
+    if (displayCount > 0 && !dynamicFavicon) {
+      title = `(${displayCount}) ${title}`;
+    }
+
+    document.title = title;
+  },
+
+  _renderFavicon() {
+    if (this.currentUser && this.currentUser.dynamic_favicon) {
+      let url = this.siteSettings.site_favicon_url;
+
+      // Since the favicon is cached on the browser for a really long time, we
+      // append the favicon_url as query params to the path so that the cache
+      // is not used when the favicon changes.
+      if (/^http/.test(url)) {
+        url = getURL("/favicon/proxied?" + encodeURIComponent(url));
+      }
+
+      new window.Favcount(url).set(this._displayCount());
+    }
+  }
+});

test/javascripts/services/document-title-test.js
@@ -1,0 +1,66 @@
+import { discourseModule } from "helpers/qunit-helpers";
+import { currentUser } from "helpers/qunit-helpers";
+
+discourseModule("service:document-title", {
+  beforeEach() {
+    this.documentTitle = this.container.lookup("service:document-title");
+    this.documentTitle.currentUser = null;
+    this.container.lookup("session:main").hasFocus = true;
+  },
+  afterEach() {
+    this.documentTitle.reset();
+  }
+});
+
+QUnit.test("it updates the document title", function(assert) {
+  this.documentTitle.setTitle("Test Title");
+  assert.equal(document.title, "Test Title", "title is correct");
+});
+
+QUnit.test(
+  "it doesn't display notification counts for anonymous users",
+  function(assert) {
+    this.documentTitle.setTitle("test notifications");
+    this.documentTitle.updateNotificationCount(5);
+    assert.equal(document.title, "test notifications");
+    this.documentTitle.updateFocus(false);
+    this.documentTitle.updateNotificationCount(6);
+    assert.equal(document.title, "test notifications");
+  }
+);
+
+QUnit.test("it displays notification counts for logged in users", function(
+  assert
+) {
+  this.documentTitle.currentUser = currentUser();
+  this.documentTitle.currentUser.dynamic_favicon = false;
+  this.documentTitle.setTitle("test notifications");
+  this.documentTitle.updateNotificationCount(5);
+  assert.equal(document.title, "test notifications");
+  this.documentTitle.updateFocus(false);
+  this.documentTitle.updateNotificationCount(6);
+  assert.equal(document.title, "(6) test notifications");
+  this.documentTitle.updateFocus(true);
+  assert.equal(document.title, "test notifications");
+});
+
+QUnit.test(
+  "it doesn't increment background context counts when focused",
+  function(assert) {
+    this.documentTitle.setTitle("background context");
+    this.documentTitle.updateFocus(true);
+    this.documentTitle.incrementBackgroundContextCount();
+    assert.equal(document.title, "background context");
+  }
+);
+
+QUnit.test("it increments background context counts when not focused", function(
+  assert
+) {
+  this.documentTitle.setTitle("background context");
+  this.documentTitle.updateFocus(false);
+  this.documentTitle.incrementBackgroundContextCount();
+  assert.equal(document.title, "(1) background context");
+  this.documentTitle.updateFocus(true);
+  assert.equal(document.title, "background context");
+});

yellowflash2041/Assassin-World

app/assets/javascripts/discourse/app/components/d-document.js
@@ -1,0 +1,58 @@
+import Component from "@ember/component";
+import { bind } from "@ember/runloop";
+import { inject as service } from "@ember/service";
+
+export default Component.extend({
+  _boundFocusChange: null,
+  tagName: "",
+  documentTitle: service(),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.documentTitle.setTitle(document.title);
+    this._boundFocusChange = bind(this, this._focusChanged);
+    document.addEventListener("visibilitychange", this._boundFocusChange);
+    document.addEventListener("resume", this._boundFocusChange);
+    document.addEventListener("freeze", this._boundFocusChange);
+    this.session.hasFocus = true;
+
+    this.appEvents.on("notifications:changed", this, this._updateNotifications);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+
+    document.removeEventListener("visibilitychange", this._boundFocusChange);
+    document.removeEventListener("resume", this._boundFocusChange);
+    document.removeEventListener("freeze", this._boundFocusChange);
+    this._boundFocusChange = null;
+
+    this.appEvents.off(
+      "notifications:changed",
+      this,
+      this._updateNotifications
+    );
+  },
+
+  _updateNotifications() {
+    if (!this.currentUser) {
+      return;
+    }
+
+    this.documentTitle.updateNotificationCount(
+      this.currentUser.unread_notifications +
+        this.currentUser.unread_high_priority_notifications
+    );
+  },
+
+  _focusChanged() {
+    if (document.visibilityState === "hidden") {
+      if (this.session.hasFocus) {
+        this.documentTitle.setFocus(false);
+      }
+    } else if (!this.hasFocus) {
+      this.documentTitle.setFocus(true);
+    }
+  }
+});

app/assets/javascripts/discourse/app/services/document-title.js
@@ -1,0 +1,105 @@
+import Service from "@ember/service";
+import { inject as service } from "@ember/service";
+import getURL from "discourse-common/lib/get-url";
+
+export default Service.extend({
+  appEvents: service(),
+  contextCount: null,
+  notificationCount: null,
+  _title: null,
+  _backgroundNotify: null,
+
+  init() {
+    this._super(...arguments);
+    this.reset();
+  },
+
+  reset() {
+    this.contextCount = 0;
+    this.notificationCount = 0;
+    this._title = null;
+    this._backgroundNotify = null;
+  },
+
+  getTitle() {
+    return this._title;
+  },
+
+  setTitle(title) {
+    this._title = title;
+    this._renderTitle();
+  },
+
+  setFocus(focus) {
+    let { session } = this;
+
+    session.hasFocus = focus;
+
+    if (session.hasFocus && this._backgroundNotify) {
+      this.updateContextCount(0);
+    }
+    this._backgroundNotify = false;
+
+    if (session.hasFocus) {
+      this.notificationCount = 0;
+    }
+    this.appEvents.trigger("discourse:focus-changed", session.hasFocus);
+    this._renderTitle();
+  },
+
+  updateContextCount(count) {
+    this.contextCount = count;
+    this._renderTitle();
+  },
+
+  updateNotificationCount(count) {
+    if (!this.session.hasFocus) {
+      this.notificationCount = count;
+      this._renderFavicon();
+      this._renderTitle();
+    }
+  },
+
+  incrementBackgroundContextCount() {
+    if (!this.session.hasFocus) {
+      this._backgroundNotify = true;
+      this.contextCount += 1;
+      this._renderFavicon();
+      this._renderTitle();
+    }
+  },
+
+  _displayCount() {
+    return this.currentUser &&
+      this.currentUser.title_count_mode === "notifications"
+      ? this.notificationCount
+      : this.contextCount;
+  },
+
+  _renderTitle() {
+    let title = this._title || this.siteSettings.title;
+
+    let displayCount = this._displayCount();
+    let dynamicFavicon = this.currentUser && this.currentUser.dynamic_favicon;
+    if (displayCount > 0 && !dynamicFavicon) {
+      title = `(${displayCount}) ${title}`;
+    }
+
+    document.title = title;
+  },
+
+  _renderFavicon() {
+    if (this.currentUser && this.currentUser.dynamic_favicon) {
+      let url = this.siteSettings.site_favicon_url;
+
+      // Since the favicon is cached on the browser for a really long time, we
+      // append the favicon_url as query params to the path so that the cache
+      // is not used when the favicon changes.
+      if (/^http/.test(url)) {
+        url = getURL("/favicon/proxied?" + encodeURIComponent(url));
+      }
+
+      new window.Favcount(url).set(this._displayCount());
+    }
+  }
+});

test/javascripts/services/document-title-test.js
@@ -1,0 +1,66 @@
+import { discourseModule } from "helpers/qunit-helpers";
+import { currentUser } from "helpers/qunit-helpers";
+
+discourseModule("service:document-title", {
+  beforeEach() {
+    this.documentTitle = this.container.lookup("service:document-title");
+    this.documentTitle.currentUser = null;
+    this.container.lookup("session:main").hasFocus = true;
+  },
+  afterEach() {
+    this.documentTitle.reset();
+  }
+});
+
+QUnit.test("it updates the document title", function(assert) {
+  this.documentTitle.setTitle("Test Title");
+  assert.equal(document.title, "Test Title", "title is correct");
+});
+
+QUnit.test(
+  "it doesn't display notification counts for anonymous users",
+  function(assert) {
+    this.documentTitle.setTitle("test notifications");
+    this.documentTitle.updateNotificationCount(5);
+    assert.equal(document.title, "test notifications");
+    this.documentTitle.setFocus(false);
+    this.documentTitle.updateNotificationCount(6);
+    assert.equal(document.title, "test notifications");
+  }
+);
+
+QUnit.test("it displays notification counts for logged in users", function(
+  assert
+) {
+  this.documentTitle.currentUser = currentUser();
+  this.documentTitle.currentUser.dynamic_favicon = false;
+  this.documentTitle.setTitle("test notifications");
+  this.documentTitle.updateNotificationCount(5);
+  assert.equal(document.title, "test notifications");
+  this.documentTitle.setFocus(false);
+  this.documentTitle.updateNotificationCount(6);
+  assert.equal(document.title, "(6) test notifications");
+  this.documentTitle.setFocus(true);
+  assert.equal(document.title, "test notifications");
+});
+
+QUnit.test(
+  "it doesn't increment background context counts when focused",
+  function(assert) {
+    this.documentTitle.setTitle("background context");
+    this.documentTitle.setFocus(true);
+    this.documentTitle.incrementBackgroundContextCount();
+    assert.equal(document.title, "background context");
+  }
+);
+
+QUnit.test("it increments background context counts when not focused", function(
+  assert
+) {
+  this.documentTitle.setTitle("background context");
+  this.documentTitle.setFocus(false);
+  this.documentTitle.incrementBackgroundContextCount();
+  assert.equal(document.title, "(1) background context");
+  this.documentTitle.setFocus(true);
+  assert.equal(document.title, "background context");
+});